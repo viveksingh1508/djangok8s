3c41029e62360f08c2e6580f8f4f297d2d641049
vivek singh <viveksingh00747@gmail.com>
2025-04-10 23:48:34 +0530

    setting up .env file

--- web/django_k8s/wsgi.py
+++ web/django_k8s/wsgi.py
@@ -15,7 +15,7 @@ from django.core.wsgi import get_wsgi_application
 CURRENT_DIR = pathlib.Path(__file__).resolve().parent
 BASE_DIR = CURRENT_DIR.parent
 ENV_FILE_PATH = BASE_DIR / ".env"
-dotenv.read_dotenv(str(ENV_FILE_PATH))
+dotenv.load_dotenv(str(ENV_FILE_PATH))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_k8s.settings")
 
 application = get_wsgi_application()
